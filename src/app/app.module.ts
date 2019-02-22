34c18399d9e78c6d3e6b6796052241611fdc153b
Karel Vervaeke <karel@vervaeke.info>
2019-02-22 14:13:31 +0100

    Revert to waves demo from mdboostrap website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {AccordionModule, MDBBootstrapModule} from "ng-uikit-pro-standard";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
